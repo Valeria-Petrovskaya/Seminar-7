b534b4a56eb94e8d6c8778f9a4541307209efae3
Lera <miss_val@bk.ru>
2022-12-19 09:45:33 +0300

    Задача 50

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[,] matrix = new int[3, 4];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(); // [1, 10]
+         //Console.WriteLine(matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+Console.WriteLine("Введите координату x: ");
+int xcoord = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y: ");
+int ycoord = Convert.ToInt32(Console.ReadLine());
+
+if (xcoord <= matrix.GetLength(0) && ycoord <= matrix.GetLength(1))
+{
+    Console.WriteLine(matrix[xcoord, ycoord]);
+} else
+{
+    Console.WriteLine("Такого элемента нет");
+}
