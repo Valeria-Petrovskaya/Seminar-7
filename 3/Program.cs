ab64c48ab0c8ce5d39482a4bdf335a452b05ab4f
Lera <miss_val@bk.ru>
2022-12-19 09:45:57 +0300

    Задача 52

---
+++ 3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[,] matrix = new int[3, 4];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(); // [1, 10]
+         //Console.WriteLine(matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Столбец {j + 1} = {sum / matrix.GetLength(0)}");
+    }
