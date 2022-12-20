a39e2449c625dc89f91a5b723e5084a1d07c6d41
Lera <miss_val@bk.ru>
2022-12-20 07:55:10 +0300

    Задача 47 испр

---
+++ 47/Program.cs
@@ -0,0 +1,11 @@
+﻿double [,] matrix = new double[3, 4];
+    for ( int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            //matrix[i, j] = new Random().Next(); // [1, 10]
+            matrix[i, j] = new Random().NextDouble();
+         Console.WriteLine(matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
